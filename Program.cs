1a931ad7627b5ed0b0ac6769a3f46cb3a983992f
John Erickson <john.t.erickson@gmail.com>
2019-11-10 20:32:01 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace jcomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter(File.OpenWrite(args[2])))
+            {
+                sw.WriteLine("v2.0 raw");
+                foreach(string line in File.ReadAllLines(args[1]))
+                {
+
+                }
+            }
+        }
+    }
+}
